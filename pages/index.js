e624c8b90296afc8b15785b32ba207db53afbaba
Theo Villeneuve <theovilleneuve.pro@gmail.com>
2023-07-29 19:53:03 +0200

    index page design

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,57 @@
-import styles from '../styles/Index.module.css';
-import Header from '../components/Header';
-import Link from 'next/link';
+import styles from "../styles/Index.module.css";
+import Header from "../components/Header";
+import Link from "next/link";
 
 export default function Index() {
   return (
-  <>
-    <Header/>
+    <>
+      <Header />
 
-    <div className={styles.part1}>
-
-      <div className={styles.logoTheo}>
-
-        <div className={styles.logoImg}>
-
-          <img 
-            src={'/logo-THEOVILLENEUVE.jpeg'} 
-            alt='logo THEOVILLENEUVE' 
-            style={{
-            objectFit: 'cover',
-            width: '100%',
-            height: '100%'
-            }}
-          />
-
-          <div className={styles.btnContainer}>
-              <Link href="/whenIWasDesigner"><button className={styles.btn}>PROJETS</button></Link>
-              <Link href="/clan"><button className={styles.btn}>ME CONTACTER</button></Link>
+      <div className={styles.part1}>
+        <div className={styles.leftContainer}>
+          <div className={styles.logoImg}>
+            <img
+              src={"/logo-THEOVILLENEUVE.jpeg"}
+              alt="logo THEOVILLENEUVE"
+              style={{
+                objectFit: "cover",
+                width: "100%",
+                height: "100%",
+              }}
+            />
           </div>
 
+          <div className={styles.textContainer}>
+            <h1> DÉVELOPPEUR FULLSTACK</h1>
+            <h2>
+              FRONTEND : <br /> Javascript (React/React native), HTML, CSS
+            </h2>
+            <h2>
+              BACKEND : <br /> Express, MongoDB
+            </h2>
+          </div>
+          <Link href="/clan">
+            <button className={styles.btn2}>ME CONTACTER</button>
+          </Link>
+        </div>
+
+        <div className={styles.rightContainer}>
+
+          <h1
+          style={{
+            color: "white",
+          }}> EN VOIR + :</h1>
+          
+          <div className={styles.btnContainer}>
+            <Link href="/whenIWasDesigner">
+              <button className={styles.btn}>COLLECTIONS</button>
+            </Link>
+            <Link href="/clan">
+              <button className={styles.btn}>PROJETS</button>
+            </Link>
+          </div>
         </div>
       </div>
-
-      <div className={styles.textContainer}>
-        <h1> DÉVELOPPEUR FULLSTACK</h1>
-        <h2> FRONTEND : Javascript (React/React native), HTML, CSS</h2>
-        <h2> BACKEND : Express, MongoDB</h2>
-      </div>
-
-    </div>
-  </>
-  )
+    </>
+  );
 }
