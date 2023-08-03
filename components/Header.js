ddda614f765af7149332979361d2d6f60cbb980a
Theo Villeneuve <theovilleneuve.pro@gmail.com>
2023-08-03 19:21:59 +0200

    MAJ background img on indexPage

--- components/Header.js
+++ components/Header.js
@@ -27,8 +27,9 @@ export default function Header(props) {
       </div>
 
       <div className={styles.menuContainer}>
-        <CollectionsMenu />
         <ProjectsMenu />
+        <CollectionsMenu />
+        
         <p
           style={{
             display: "flex",
